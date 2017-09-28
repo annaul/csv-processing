eec620df0136bcbf56fccad6f1bcc9494b25b098
Anna Ulanova <anetteua0@gmail.com>
2017-09-28 14:38:14 -0700

    sorting by retailer

--- public/app.js
+++ public/app.js
@@ -1,13 +1,12 @@
 'use strict';
 
 // --- get data from file ---
-
 function fileLoaded(reader) {
   var data = reader.result
-  console.log(data);
   var lines = data.split('\n');
-  console.log(lines[1].split(',')[0]);
+  console.log(lines.length);
   restructureData(data, lines);
+  sortbyName(statement);
 }
 
 $('input').change(function() {
@@ -22,14 +21,13 @@ $('input').change(function() {
 var statement = [];
 
 function restructureData(data, lines) {
-
   var Transaction = function(date, transaction, name, memo, amount) {
     this.date = date;
     this.transaction = transaction;
     this.name = name;
     this.memo = memo;
     this.amount = amount;
-  };
+  }
 
   for (var i = 1; i < lines.length; i++) {
     var oneTransaction = lines[i].split(',');
@@ -40,3 +38,18 @@ function restructureData(data, lines) {
 }
 
 // --- manipulate statement array ---
+var names = {};
+
+function sortbyName(statement) {
+  for (var i = 0; i < statement.length; i++) {
+    if (!statement[i].name) { break; }; 
+    var retailer = statement[i].name.split(' ')[0].split(/[^A-Za-z]/)[0].toLowerCase();
+    if (!names.hasOwnProperty(retailer)) {
+      names[retailer] = retailer = [];
+    }
+    console.log('---', i ,retailer);
+    names[statement[i].name.split(' ')[0].split(/[^A-Za-z]/)[0].toLowerCase()].push(statement[i]);
+  }
+  console.log(names);
+  return names;
+}
