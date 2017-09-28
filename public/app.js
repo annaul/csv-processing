e22e610133614afd9ef8096793b76a22317ff694
Anna Ulanova <anetteua0@gmail.com>
2017-09-27 23:01:38 -0700

    s

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function fileLoaded(reader) {
+  console.log(reader.result);
+}
+
+$('input').change(function() {
+  var file = $('#input')[0].files[0];
+  var reader = new FileReader();
+
+  reader.addEventListener('loadend', function() { fileLoaded(reader); }, false)
+  reader.readAsText(file);
+});
