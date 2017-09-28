635d1cec8c7d8e944f75f7b0f474feabe1aede6e
Anna Ulanova <anetteua0@gmail.com>
2017-09-27 21:44:33 -0700

    s

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('./public'));
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: './public' });
+});
+
+app.listen(PORT, function(){
+  console.log('Server is up and running on port 5000');
+})
