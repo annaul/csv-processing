0cf308a0ffbb0e75e727439b2974b67ea3cdfbe9
Anna Ulanova <anetteua0@gmail.com>
2017-09-28 10:25:12 -0700

    creating json

--- public/app.js
+++ public/app.js
@@ -4,7 +4,10 @@
 
 function fileLoaded(reader) {
   var data = reader.result
-  var lines = data.split('\n'));
+  console.log(data);
+  var lines = data.split('\n');
+  console.log(lines[1].split(',')[0]);
+  restructureData(data, lines);
 }
 
 $('input').change(function() {
@@ -16,14 +19,24 @@ $('input').change(function() {
 });
 
 // --- create array of objects ---
-var Transaction = function(date, transaction, name, memo, amount) {
-  this.date = date;
-  this.transaction = transaction;
-  this.name = name;
-  this.memo = memo;
-  this.amount = amount;
-};
+var statement = [];
 
-for (var i = 1; i < lines; i++) {
-  
+function restructureData(data, lines) {
+
+  var Transaction = function(date, transaction, name, memo, amount) {
+    this.date = date;
+    this.transaction = transaction;
+    this.name = name;
+    this.memo = memo;
+    this.amount = amount;
+  };
+
+  for (var i = 1; i < lines.length; i++) {
+    var oneTransaction = lines[i].split(',');
+    var transactionObj = new Transaction(oneTransaction[0], oneTransaction[1], oneTransaction[2], oneTransaction[3], oneTransaction[4]);
+    statement.push(transactionObj);
+  }
+  return statement;
 }
+
+// --- manipulate statement array ---
