f318a4d86609f676be0152d372bf04b574a451c9
Anna Ulanova <anetteua0@gmail.com>
2017-09-27 23:39:02 -0700

    getting and cutting

--- public/app.js
+++ public/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
+// --- get data from file ---
+
 function fileLoaded(reader) {
-  console.log(reader.result);
+  var data = reader.result
+  var lines = data.split('\n'));
 }
 
 $('input').change(function() {
@@ -11,3 +14,16 @@ $('input').change(function() {
   reader.addEventListener('loadend', function() { fileLoaded(reader); }, false)
   reader.readAsText(file);
 });
+
+// --- create array of objects ---
+var Transaction = function(date, transaction, name, memo, amount) {
+  this.date = date;
+  this.transaction = transaction;
+  this.name = name;
+  this.memo = memo;
+  this.amount = amount;
+};
+
+for (var i = 1; i < lines; i++) {
+  
+}
