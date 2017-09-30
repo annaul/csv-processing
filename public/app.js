a8cafb24f2344ec2f142b1953a88d25300aca4f5
Anna Ulanova <anetteua0@gmail.com>
2017-09-30 00:13:55 -0700

    sorting by days, filtering out repeats

--- public/app.js
+++ public/app.js
@@ -9,6 +9,7 @@ function fileLoaded(reader) {
   sortbyName(statement);
   findGroupSum(names);
   sortbySum(names);
+  sortbyDay(statement);
 }
 
 $('input').change(function() {
@@ -66,18 +67,54 @@ function findGroupSum(names) {
     var sum = 0;
     for (var i = 0; i < names[key].length; i ++) {
       sum+= parseInt(names[key][i]['amount']);
-    }
+    };
     var statsObj = { sum: sum };
     names[key].push(statsObj);
-  }
-}
+  };
+};
 
+var sortedBySum = [];
 function sortbySum(names) {
-  var sortedBySum = [];
   for (var key in names) {
     sortedBySum.push(names[key]);
   }
   sortedBySum.sort(function(a, b) {
     return a[a.length - 1]['sum'] - b[b.length -1]['sum'];
   });
+};
+
+function sortbyDay(statement) {
+  var mon = {number: 0, names: []}; var tue = {number: 0, names: []}; var wed = {number: 0, names: []}; var thur = {number: 0, names: []}; var fri = {number: 0, names: []}; var sat = {number: 0, names: []}; var sun = {number: 0, names: []};
+
+  for (var i = 0; i < statement.length; i++) {
+    if (new Date(statement[i].date).getDay() === 1) {
+      mon.number += 1;
+      if (mon.names.indexOf(statement[i].name) === -1) mon.names.push(statement[i].name);
+    }
+    if (new Date(statement[i].date).getDay() === 2) {
+      tue.number += 1;
+      if (tue.names.indexOf(statement[i].name) === -1) tue.names.push(statement[i].name);
+    }
+    if (new Date(statement[i].date).getDay() === 3) {
+      wed.number += 1;
+      if (wed.names.indexOf(statement[i].name) === -1) wed.names.push(statement[i].name);
+    }
+    if (new Date(statement[i].date).getDay() === 4) {
+      thur.number += 1;
+      if (thur.names.indexOf(statement[i].name) === -1) thur.names.push(statement[i].name);
+    }
+    if (new Date(statement[i].date).getDay() === 5) {
+      fri.number += 1;
+      if (fri.names.indexOf(statement[i].name) === -1) fri.names.push(statement[i].name);
+    }
+    if (new Date(statement[i].date).getDay() === 6) {
+      sat.number += 1;
+      if (sat.names.indexOf(statement[i].name) === -1) sat.names.push(statement[i].name);
+    }
+    if (new Date(statement[i].date).getDay() === 0) {
+      sun.number += 1;
+      if (sun.names.indexOf(statement[i].name) === -1) sun.names.push(statement[i].name);
+    }
+  }
+  console.log('mon ', mon.number, mon.names, 'tue ', tue.number, tue.names, 'wed ', wed.number, wed.names, 'thur ', thur.number, thur.names, 'fri ', fri.number, fri.names, 'sat ', sat.number, sat.names, 'sun ', sun.number, sun.names);
 };
