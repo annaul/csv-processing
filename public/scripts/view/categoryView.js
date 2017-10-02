4ef6580be375489951114aefeee80b03d8f14a16
Anna Ulanova <anetteua0@gmail.com>
2017-10-01 23:53:00 -0700

    s

--- public/scripts/view/categoryView.js
+++ public/scripts/view/categoryView.js
@@ -8,8 +8,9 @@
       var statement = module.fileInput.statement;
       var names = module.fileInput.names;
       showRetailers(names);
-      chooseCategory(names);
       autoAssignCategory(names);
+      showCategories(names);
+      chooseCategory(names);
     });
   });
 
@@ -17,7 +18,7 @@
   function showRetailers(names) {
     var retailerCategories = [
       'clothing', 'groceries', 'entertainment', 'travel', 'donation',
-      'home', 'utilities', 'education', 'beauty', 'eating out', 'amazon'
+      'home', 'utilities', 'education', 'health', 'beauty', 'eating out', 'amazon'
     ];
     var template = Handlebars.compile($('#category-template').html());
     for (var key in names) {
@@ -32,9 +33,36 @@
     }
   }
 
+  function showCategories(names) {
+    var retailersList = $('.retailer').siblings();
+    for (var i = 0; i < retailersList.length; i++) {
+      var keys = Object.keys(names)
+      var autoCategory = names[keys[i]][names[keys[i]].length - 1]['category']
+      if (autoCategory) {
+        $(retailersList[i]).append('<span>' + autoCategory + '</span>')
+      }
+    }
+  }
+
   function autoAssignCategory(names) {
     for (var key in names) {
-      
+      if (names[key][0]['name'].match(/URBAN|HAUTLK|ANTHR|NORDSTR|OLDNA|GAP|LOFT/g)) {
+        names[key][names[key].length - 1].category = 'clothing';
+      } else if (names[key][0]['name'].match(/MARKET|WHOLEFDS|TRADER JOE'S|SAFEWAY|QFC|PCC/g)) {
+        names[key][names[key].length - 1].category = 'groceries';
+      } else if (names[key][0]['name'].match(/GOLDSTAR/g)) {
+        names[key][names[key].length - 1].category = 'entertainment';
+      } else if (names[key][0]['name'].match(/TRACY IRVING|SEPHORA|RECOOP/g)) {
+        names[key][names[key].length - 1].category = 'beauty';
+      } else if (names[key][0]['name'].match(/COMCAST/g)) {
+        names[key][names[key].length - 1].category = 'utilities';
+      } else if (names[key][0]['name'].match(/YOUSEFIAN/g)) {
+        names[key][names[key].length - 1].category = 'health';
+      } else if (names[key][0]['name'].match(/AMAZON/g)) {
+        names[key][names[key].length - 1].category = 'amazon';
+      } else {
+        names[key][names[key].length - 1].category = null;
+      }
     }
   }
 
