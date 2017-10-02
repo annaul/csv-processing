079307abcf2dfdc72384077be216a7d5fa04fcec
Anna Ulanova <anetteua0@gmail.com>
2017-10-01 20:36:31 -0700

    choose category

--- public/scripts/view/categoryView.js
+++ public/scripts/view/categoryView.js
@@ -7,23 +7,43 @@
     $.when( $.ajax( 'fileInput.js' )).then( function() {
       var statement = module.fileInput.statement;
       var names = module.fileInput.names;
-      console.log(names);
       showRetailers(names);
+      chooseCategory(names);
+      autoAssignCategory(names);
     });
   });
 
-  var template = Handlebars.compile($('#category-template').html());
-
-  var context = { st: 'hello!' };
-  var html = template(context);
-
-  $('.categories').append(html);
-
   //show a list of retailers to assign categories
   function showRetailers(names) {
+    var retailerCategories = [
+      'clothing', 'groceries', 'entertainment', 'travel', 'donation',
+      'home', 'utilities', 'education', 'beauty', 'eating out', 'amazon'
+    ];
+    var template = Handlebars.compile($('#category-template').html());
     for (var key in names) {
-      console.log(key, names[key]);
+      var context = { retailer: names[key][0]['name'] };
+      var html = template(context);
+      $('.categories').append(html);
+      $('.retailer:last-child').append('<select></select>').attr('id', key);
+      for (var i = 0; i < retailerCategories.length; i++) {
+        $('.retailer:last-child select').append(
+          '<option value=\"' + retailerCategories[i] + '\">' + retailerCategories[i] + '</option>');
+      }
     }
+  }
+
+  function autoAssignCategory(names) {
+    for (var key in names) {
+      
+    }
+  }
+
+  function chooseCategory(names) {
+    $('select').change(function() {
+      var cat = $(this).parent().prop('id')
+      names[cat][names[cat].length - 1].category = $(this).val();
+      console.log(names[cat][names[cat].length - 1]);
+    });
   }
 
   module.categoryView = categoryView;
