3e9397977a3053da078d93a57da7f92ed95c34ca
Anna Ulanova <anetteua0@gmail.com>
2017-10-06 12:09:02 -0700

    vega connecting

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('./public'));
 
-// app.get('*', function(request, response) {
-//   // console.log('New request:', request.url);
-//   response.sendFile('index.html', { root: './public' });
-// });
+app.get('*', function(request, response) {
+  // console.log('New request:', request.url);
+  response.sendFile('index.html', { root: './public' });
+});
 
 app.listen(PORT, function(){
   console.log('Server is up and running on port 5000');
